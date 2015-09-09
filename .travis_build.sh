8a14825d1e19824b076b388408ad1a9c08bfa2d1
Jim Rosenblum <jrosenblum@carelogistics.com>
2015-09-08 21:34:45 -0400

    no message

--- .travis_build.sh
+++ .travis_build.sh
@@ -2,7 +2,7 @@
 
 wget https://s3.amazonaws.com/rebar3/rebar3
 
-cp -f travis/rebar.config.travis rebar.config
+cp -f travis/rebar.config rebar.config
 cp -f travis/jc.app.src src/jc.app.src  
 
 chmod a+x ./rebar3
