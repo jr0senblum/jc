cc9d9463265b705d2e4011981045a9c5733a0a31
Jim Rosenblum <jrosenblum@carelogistics.com>
2015-09-14 21:58:38 -0400

    Improved the protocol

--- src/jc_protocol.erl
+++ src/jc_protocol.erl
@@ -3,11 +3,38 @@
 %%% @copyright (C) 2015, Jim Rosenblum
 %%% @doc 
 %%% This is the back-end, protocol gen_server that, utilizing ranch, provides a
-%%% very simple binary protocol to interact with jc.
-%%% Message format is [size]command. The size does not include [size]. 
+%%% very simple BINARY STRING protocol to interact with jc. All of the lines 
+%%% shown below are assumed to be BINARY STRINGS.
+%%%
+%%% The CONNECT frame:
+%%% CONNECT\r\n
+%%% VERSION:1.0\r\n
+%%% CRLF:TRUE|FALSE\r\n
+%%% USERNAME:username\r\n
+%%% PASSWORD:password\r\n
+%%% 
+%%% CRLF MUST be TRUE if COMMAND frames terminate with \r\n. Otherwise,
+%%% Otherwise, CRLF MUST be FALSE and lines MUST NOT be terminated with \r\n. 
+%%% Server responds to CONNECT frames with BINARY STRINGS.
+%%%
+%%% [11]VERSION:1.0
+%%% or
+%%% [5]error and the socket will be closed.
+%%%
+%%% COMMAND frames consist of the byte_size enclosed in brackets
+%%% followed by the command. If CRLF was set to TRUE when connecting than
+%%% COMMAND lines MUST be terminated with \r\n; otherwise they MUST NOT.
+%%% The supplied byte_size does not include the [byte_size] portion of the frame
+%%% nor does it include any terminating \r\n characters.
+%%%
 %%% For example:
 %%% [15jc:put(bed,1,1)
-%%% Responses follow the same format:
+%%%
+%%% Closing a CONNECTION is a COMMAND frame as follows
+%%% [5]CLOSE
+%%%
+%%% RESPONSE frames are identical to COMMAND frames, but NEVER end with \r\n.
+%%% Like all other lines, RESPONSE lines are BINARY STRINGS
 %%% [6]{ok,1}
 %%%
 %%% @end
@@ -38,10 +65,17 @@
 	D >= <<$0>> andalso 
 	D=< <<$9>>).
 
--record(jc_protocol_state, {socket, 
-			    transport, 
-			    command=undefined, 
-			    size}).
+-record(jc_pro_state, {socket, 
+			    transport           :: 'jc_protocol', 
+			    connected = false   :: boolean(),
+			    have_size = false   :: boolean(),
+			    command = undefined :: term(),
+			    size = undefined    :: undefined | 
+						   non_neg_integer(),
+			    crlf = undefined    :: boolean() | undefined,
+			    username = <<"">>   :: binary(),
+			    password = <<"">>   :: binary(),
+			    version = <<"1.0">> :: binary()}).
 
 
 
@@ -79,31 +113,32 @@ init([]) -> {ok, undefined}.
 %%
 -spec init(ranch:ref(), any(), jc_protocol, [Port::integer()]) -> none().
 
-init(Ref, Socket, Transport, _Opts = [Port]) ->
+init(Ref, S, T, _Opts = [Port]) ->
     ok = proc_lib:init_ack({ok, self()}),
     ok = ranch:accept_ack(Ref),
-    ok = Transport:setopts(Socket, [{active, once}]),
+    ok = T:setopts(S, [{active, once}]),
     lager:debug("~p(~p): up and listening on ~p.",
 	       [?MODULE, self(), Port]),
 
     gen_server:enter_loop(?MODULE, [],
-			  #jc_protocol_state{socket=Socket, transport=Transport},
+			  #jc_pro_state{socket = S, 
+					     transport = T,
+					     connected = false,
+					     have_size = false,					     
+					     size = undefined,
+					     command = undefined},
 			  ?TIMEOUT).
-
 
 %% -----------------------------------------------------------------------------
 %% @private Handle info messages: Socket messages and jc_psub subscription 
 %% messages.
 %%
--spec handle_info(any(),#jc_protocol_state{}) -> {noreply,#jc_protocol_state{}}.
+-spec handle_info(any(),#jc_pro_state{}) -> {noreply,#jc_pro_state{}}.
 
-handle_info({tcp, S, Data}, State = #jc_protocol_state{socket=S,
-						       transport=T,
-						       command=C,
-						       size=Size}) ->
+handle_info({tcp, S, Data}, State = #jc_pro_state{socket=S, transport=T})->
     T:setopts(S, [{active, once}]),
-    {NewC, NewSize} = collect(T, S, Data, C, Size),
-    {noreply, State#jc_protocol_state{command = NewC, size = NewSize}, ?TIMEOUT};
+    NewState = protocol(Data, State),
+    {noreply, NewState, ?TIMEOUT};
 
 handle_info({tcp_closed, _Socket}, State) ->
 	{stop, normal, State};
@@ -114,7 +149,7 @@ handle_info({tcp_error, _, Reason}, State) ->
 handle_info(timeout, State) ->
 	{stop, normal, State};
 
-handle_info(Msg, State = #jc_protocol_state{socket=S, transport = T}) ->
+handle_info(Msg, State = #jc_pro_state{socket=S, transport = T}) ->
     T:send(S, marshal(Msg)),
     {noreply, State, ?TIMEOUT}.
 
@@ -122,8 +157,8 @@ handle_info(Msg, State = #jc_protocol_state{socket=S, transport = T}) ->
 %% -----------------------------------------------------------------------------
 %% @private Hande call messages.
 %%
--spec handle_call(term(), {pid(), _}, #jc_protocol_state{}) -> 
-			                         {reply, ok, #jc_protocol_state{}}.
+-spec handle_call(term(), {pid(), _}, #jc_pro_state{}) -> 
+			                         {reply, ok, #jc_pro_state{}}.
 handle_call(_Request, _From, State) ->
 	{reply, ok, State}.
 
@@ -132,7 +167,7 @@ handle_call(_Request, _From, State) ->
 %% -----------------------------------------------------------------------------
 %% @private Handle cast messages.
 %%
--spec handle_cast(any(), #jc_protocol_state{}) -> {noreply, #jc_protocol_state{}}.
+-spec handle_cast(any(), #jc_pro_state{}) -> {noreply, #jc_pro_state{}}.
 
 handle_cast(_Msg, State) ->
 	{noreply, State}.
@@ -142,7 +177,7 @@ handle_cast(_Msg, State) ->
 %% -----------------------------------------------------------------------------
 %% @private Terminate server.
 %%
--spec terminate(any(), #jc_protocol_state{}) -> any().
+-spec terminate(any(), #jc_pro_state{}) -> any().
 
 terminate(_Reason, _State) ->
 	ok.
@@ -151,8 +186,8 @@ terminate(_Reason, _State) ->
 %% -----------------------------------------------------------------------------
 %% @private Convert process state when code is changed.
 %%
--spec code_change(term(), #jc_protocol_state{}, any()) -> 
-			                              {ok, #jc_protocol_state{}}.
+-spec code_change(term(), #jc_pro_state{}, any()) -> 
+			                              {ok, #jc_pro_state{}}.
 
 code_change(_OldVsn, State, _Extra) ->
 	{ok, State}.
@@ -163,52 +198,197 @@ code_change(_OldVsn, State, _Extra) ->
 %%% Internal functions
 %%% ============================================================================
 
-collect(T, S, <<"[", B/binary>>, undefined, _Size) ->
-    try
-	start_command(T, S, B, <<>>)
-    catch
-	_:E ->
-	    T:send(S, marshal({error, E})),
-	    {undefined, undefined}
-    end;
 
-collect(T, S, Binary, Acc, Size) ->
+%% =============================================================================
+%% Top-level protcol parser
+%%
+protocol(B, #jc_pro_state{transport = T, socket = S} = State) ->
     try
-	Stripped = binary:part(Binary, {0, byte_size(Binary) - 2}),
-	parse(T, S, Stripped, Acc, Size)
+	proto(B, State)
     catch
-	_:E ->
-	    T:send(S, marshal({error, E})),
-	    {undefined, undefined}
+	throw:{fatal, _F} ->
+	    T:send(S, marshal(error)),
+	    self() ! {tcp_closed, S};
+	_:_ ->
+	    T:send(S, marshal(error)),
+	    reset_state(State)
+    end.
+	
+
+%% Not CONNECTED, assume we are in a CONNECT frame, looking for VERSION, CRLF
+%% USERNAME and PASSWORD segments.
+%%
+proto(B, #jc_pro_state{command=C,connected=false,transport=T,socket=S}=State) ->
+    NewState = case C of 
+		   X when X == undefined; X == version; X == crlf ->
+		       connect(B, State);
+		   username ->
+		       username(B, State);
+		   password ->
+		       password(B, State)
+	       end,
+    case NewState#jc_pro_state.connected of
+	true ->
+	    Version = NewState#jc_pro_state.version,
+	    lager:debug("~p (~p): connected ~p",
+			[?MODULE, self(), {Version,
+					   #jc_pro_state.username}]),
+	    T:send(S, <<"[11]VERSION:1.0">>);
+	false ->
+	    ok
+    end,
+    NewState;
+
+%% CONNECTED, parsing a COMMAND frame - look for the size of the COMMAND.
+proto(B,#jc_pro_state{connected=true,have_size=false,crlf=F,size=Size}=State) ->
+    Stripped = F(B),
+    NewState = case Size of
+		   undefined ->
+		       get_size(Stripped, <<>>, State);
+		   Partial ->
+		       get_size(Stripped, Partial, State)
+	       end,
+    NewState;
+
+%% Parsing COMMAND frame, size has been retrieved, accumulate the command.
+proto(B,#jc_pro_state{connected=true,have_size=true,command=Com,crlf=F}=State) ->
+    Stripped = F(B),
+    get_command(Stripped, Com, State).
+
+
+
+%% Walk the binary, accumulating the size from [N] portion of the COMMAND frame.
+get_size(<<>>, Acc, State) ->
+    State#jc_pro_state{size=Acc};
+
+get_size(<<"[", B/binary>>, Acc, State) ->
+    get_size(B, Acc, State);
+
+get_size(<<N:1/binary, B/binary>>, Acc, State) when ?DIG(N) ->
+    get_size(B, <<Acc/binary, N/binary>>, State);
+
+get_size(<<"]", B/binary>>, Acc, State) ->
+    CmdSize = binary_to_integer(Acc),
+    get_command(B, <<>>, State#jc_pro_state{size=CmdSize, have_size = true});
+get_size(_B, _A, _State) ->
+    throw({command, bad_size_segment}).
+
+
+
+% Walk the binary accumulating Size characters from the COMMAND frame, when no 
+% more characters (size = 0), execute the command.
+%
+get_command(_B, Acc, #jc_pro_state{size=0} = State) ->
+    parse(State#jc_pro_state{command = Acc});
+
+get_command(<<>>, Acc, State) ->
+    State#jc_pro_state{command=Acc};
+
+get_command(<<C:1/binary, B/binary>>, Acc, #jc_pro_state{size=Size} = State) when Size > 0 ->
+    get_command(B, <<Acc/binary, C/binary>>, State#jc_pro_state{size = Size-1}).
+
+
+% Execute the COMMAND.
+parse(#jc_pro_state{socket = S, command = Com} = State) ->
+    lager:debug("~p (~p): executing command: ~p",  
+		[?MODULE, self(), Com]),
+
+    {ok,Scanned,_} = erl_scan:string(binary_to_list(<<Com/binary, ".">>)),
+    {ok,Parsed} = erl_parse:parse_exprs(Scanned),
+
+    case Parsed of
+	[{var,1,'CLOSE'}] -> 
+	    self() ! {tcp_closed, S};
+	AST ->	
+	    AST2 = make_ast(AST),
+	    {value, _R, _} = erl_eval:exprs(AST2, []),
+	    reset_state(State)
     end.
 
 
-start_command(T, S, <<"]", B/binary>>, Acc) ->
-    CmdSize = binary_to_integer(Acc),
-    Stripped = binary:part(B, {0, byte_size(B) - 2}),
-    parse(T, S, Stripped, <<>>, CmdSize);
-	     
-start_command(T, S, <<N:1/binary, B/binary>>, Acc) when ?DIG(N) ->
-    start_command(T, S, B, <<Acc/binary, N/binary>>).
+% Convet the AST to one that sends jc_bridge the command
+% {Op, P1, P2} becomes jc_bridge ! {self(), {Op, P1, P2}}
+%
+make_ast(AST)->
+    [{tuple, 1, R}] = AST,
+    [{op,1,'!',
+      {atom,1,jc_bridge},
+      {tuple,1,
+       [{call,1,{atom,1,self},[]},
+	{tuple,1,
+	 R}]}}].
 
 
 
-parse(Transport, Socket, _B, Acc, 0) ->
-    {ok,Scanned,_} = erl_scan:string(binary_to_list(<<Acc/binary, ".">>)),
-    {ok,Parsed} = erl_parse:parse_exprs(Scanned),
-    {value, R, _} = erl_eval:exprs(Parsed, []),
-    
-    Transport:send(Socket, marshal(R)),
-    {undefined, undefined};
+% CONNECT FRAME parsing
+% 
+connect(<<"CONNECT\r\n", B/binary>>, S=#jc_pro_state{command = undefined}) ->
+    connect(B, S#jc_pro_state{command=version});
+connect(<<"VERSION:1.0\r\n", B/binary>>, S=#jc_pro_state{command=version}) ->
+    connect(B, S#jc_pro_state{version = <<"1.0">>, command=crlf});
+connect(<<"CRLF:TRUE\r\n", B/binary>>, S=#jc_pro_state{command=crlf}) ->
+    connect(B, S#jc_pro_state{command=username, crlf=fun(I) -> crlf(I) end});
+connect(<<"CRLF:FALSE\r\n", B/binary>>, S=#jc_pro_state{command=crlf}) ->
+    username(B, S#jc_pro_state{command=username, crlf=fun(I) -> I end});
+connect(<<>>, State) ->
+    State;
+connect(B, _State) ->
+    throw({fatal, {bad_connect_frame, B}}).
 
-parse(_Transport, _Socket, <<>>, Acc, Size) ->
-    {Acc, Size};
 
-parse(Transport, Socket, <<C:1/binary, B/binary>>, Acc, Size) ->
-    parse(Transport, Socket, B, <<Acc/binary, C/binary>>, Size-1).
+username(<<"USERNAME:", B/binary>>, S) ->
+    username(B, S);
+username(<<"\r\n", B/binary>>, S)->
+    password(B, S#jc_pro_state{command = password});
+username(<<C:1/binary, B/binary>>, #jc_pro_state{username = U} = S)-> 
+    username(B, S#jc_pro_state{username = <<U/binary, C:1/binary>>});
+username(<<>>, S) ->
+    S.
 
 
+password(<<"PASSWORD:", B/binary>>, S) ->
+    password(B, S);
+password(<<"\r\n", _B/binary>>, S)->
+     S#jc_pro_state{connected = true};
+password(<<C:1/binary,B/binary>>, #jc_pro_state{password = P} = S)-> 
+    password(B, S#jc_pro_state{password = <<P/binary, C:1/binary>>});
+password(<<>>, State) ->
+    State.
+
+
+
+% ==============================================================================
+% Utility functions
+% ==============================================================================
+
+
+%% Strip off [carriage-return]lf
+crlf(<<"\r\n">>) ->
+    <<>>;
+crlf(<<"\n">>) ->
+    <<>>;
+crlf(<<>>) ->
+    <<>>;
+crlf(B) when erlang:binary_part(B, byte_size(B) - 2, 2) == <<"\r\n">> ->
+    binary:part(B, {0, byte_size(B) - 2});
+
+crlf(B) when erlang:binary_part(B, byte_size(B) - 1, 1) == <<"\n">> ->
+    binary:part(B, {0, byte_size(B) - 1});
+crlf(B) ->
+    B.
+
+
+
+%% After a COMMAND frame, reset the CONNECTION (state) for next COMMAND frame.
+reset_state(S) ->
+    S#jc_pro_state{have_size = false,
+			command = undefined,
+			size = undefined}.
+
+
+% Messsage -> [Size]Message
 marshal(Message) ->
     Bin  = binary:list_to_bin(io_lib:format("~p", [Message])),
-    Size = size(Bin),
+    Size = byte_size(Bin),
     ["[",integer_to_list(Size),"]", Bin].
+
