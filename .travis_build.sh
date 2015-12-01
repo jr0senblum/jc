0a0d53bd2e18a0069f65aee98fe0fb0405c085a6
Jim Rosenblum <jrosenblum@carelogistics.com>
2015-12-01 15:53:12 -0500

    improved travis integration

--- .travis_build.sh
+++ .travis_build.sh
@@ -2,9 +2,6 @@
 
 wget https://s3.amazonaws.com/rebar3/rebar3
 
-cp -f travis/rebar.config rebar.config
-cp -f travis/jc.app.src src/jc.app.src  
-
 chmod a+x ./rebar3
 ./rebar3 unlock
 ./rebar3 update
