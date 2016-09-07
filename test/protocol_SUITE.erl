150adf96288a5ae03ded8a4e3dc1b5e39842aecf
Jim Rosenblum <jrosenblum@carelogistics.com>
2016-09-07 08:40:04 -0400

    fixed test-cases for Erlang 19

--- test/protocol_SUITE.erl
+++ test/protocol_SUITE.erl
@@ -105,13 +105,11 @@ put_get_test(_Config) ->
     <<"{\"ok\":[\"1\",\"3\",\"4\"]}">> = get_result(),
 
     t:send("{values, bed}"),
-    true = case get_result() of 
-               <<"{\"ok\":[1,3.3,[1,2,3]]}">> -> 
-                   true;
-               X ->
-                   X == <<"{\"ok\":[1,3.3,[1,2,3]]}">> 
-           end,
-
+    true = 
+        case lists:sort(maps:get(<<"ok">>,jsone:decode(get_result()))) of
+            [1,3.3,[1,2,3]] -> true
+        end,
+    
 
     t:send("{values_match, json, \"first.second=1\"}"),
     R = list_to_binary("[{\"key\":\"1\",\"value\":" ++ J1 ++ "}]"),
